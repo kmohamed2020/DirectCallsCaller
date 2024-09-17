a109c9b3818f44cefb760fbbdecef702be0c4282
Khaled Salem <khaledfawzy@outlook.com>
2024-09-17 17:23:25 +0300

    Update DirectCallsCaller.cpp

--- DirectCallsCaller.cpp
+++ DirectCallsCaller.cpp
@@ -1,4 +1,4 @@
-// ]DirectCallsCaller.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// DirectCallsCaller.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
